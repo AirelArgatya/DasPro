ec45389cf0b6da31de6fbc7367561e9c62f3b30a
AirelArgatya <airelargatya01@gmail.com>
2026-08-31 19:23:19 +0700

    Add files via upload

---
+++ DASPRO/PRAKTEK DASPRO/Latihan02.java
@@ -0,0 +1,8 @@
+public class Latihan02 {
+    public static void main(String[] args) {
+        System.out.println("Halo, nama saya adalah Airel!");
+
+        System.out.println("Belajar Java itu mudah!");
+        System.out.println("Semakin sering mencoba, semakin jago.");
+    }
+}
